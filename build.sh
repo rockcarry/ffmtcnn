385e3a85133f03b122cd994e8784f47a68ef578d
chenk <chenk@apical.com.cn>
2021-04-13 11:58:06 +0800

    修改编译脚本，可以编译生成 msvc 能使用的 .lib 文件

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ LD_FLAGS="-L$PWD/libncnn/lib -lncnn -fopenmp"
 
 g++ -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test.exe
 g++ --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.dll
-
+dlltool -l ffmtcnn.lib -d ffmtcnn.def
