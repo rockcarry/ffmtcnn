7202802f7f52b9a8039a7095c5f92881b6eaf89f
chenk <chenk@apical.com.cn>
2021-04-14 11:57:32 +0800

    修改编译脚本，在 msys2, ubuntu 和 msc33x 上都可以编译通过

--- src/build.sh
+++ src/build.sh
@@ -2,14 +2,29 @@
 
 set -e
 
-CXX_FLAGS="-I$PWD/../libncnn/include -ffunction-sections -fPIC"
-LD_FLAGS="-L$PWD/../libncnn/lib -lncnn -fopenmp -Wl,-gc-sections -Wl,-strip-all"
+CXX_FLAGS="-I$PWD/../libncnn/include -std=c++11 -ffunction-sections -fPIC"
+LD_FLAGS="-L$PWD/../libncnn/lib -lncnn -lpthread -Wl,-gc-sections -Wl,-strip-all"
 
 case "$1" in
 "")
-    g++ -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test
+    $CXX -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test
+    case "$TARGET_PLATFORM" in
+    win32)
+        $CXX --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.dll
+        dlltool -l ffmtcnn.lib -d ffmtcnn.def
+        $STRIP *.exe *.dll
+        ;;
+    ubuntu)
+        $CXX --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.so
+        $STRIP test *.so
+        ;;
+    msc33x)
+        $CXX --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.so
+        $STRIP test *.so
+        ;;
+    esac
     ;;
 clean)
-    rm -rf test test.exe out.bmp
+    rm -rf test *.so *.dll *.exe out.bmp
     ;;
 esac
