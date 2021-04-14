31bf3ca15001666296d66c04b01ee2500d7a7c6f
chenk <chenk@apical.com.cn>
2021-04-14 09:48:05 +0800

    修改编译脚本，增加 clean 功能

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@
 CXX_FLAGS="-I$PWD/libncnn/include -ffunction-sections"
 LD_FLAGS="-L$PWD/libncnn/lib -lncnn -fopenmp -Wl,-gc-sections -Wl,-strip-all"
 
-g++ -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test.exe
-g++ --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.dll
-dlltool -l ffmtcnn.lib -d ffmtcnn.def
+case "$1" in
+"")
+    g++ -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test.exe
+    g++ --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.dll
+    dlltool -l ffmtcnn.lib -d ffmtcnn.def
+    ;;
+clean)
+    rm *.lib *.dll *.exe
+    ;;
+esac
