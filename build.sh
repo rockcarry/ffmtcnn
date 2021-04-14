cea4ce289989c1598b91f2d78060e671ef281f9c
chenk <chenk@apical.com.cn>
2021-04-14 09:43:13 +0800

    修改 build.sh 脚步，调整 gcc 编译参数

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CXX_FLAGS="-I$PWD/libncnn/include"
-LD_FLAGS="-L$PWD/libncnn/lib -lncnn -fopenmp"
+CXX_FLAGS="-I$PWD/libncnn/include -ffunction-sections"
+LD_FLAGS="-L$PWD/libncnn/lib -lncnn -fopenmp -Wl,-gc-sections -Wl,-strip-all"
 
 g++ -Wall -D_TEST_ bmpfile.c ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o test.exe
 g++ --shared ffmtcnn.cpp $CXX_FLAGS $LD_FLAGS -o ffmtcnn.dll
